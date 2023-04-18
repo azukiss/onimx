9a67d9bd616a83072e317d3ef3f57cb31fa1ae2d
Farid Azhar <73160631+azukiss@users.noreply.github.com>
2023-04-18 22:22:37 +0700

    composer
    - fortify
    - spatie permissions
    - assets

--- config/app.php
+++ config/app.php
@@ -195,6 +195,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // Spatie Permissions
+        Spatie\Permission\PermissionServiceProvider::class,
+
     ],
 
     /*
