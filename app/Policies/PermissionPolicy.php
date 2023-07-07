bb0fbc330cf0dcd65ec967e87aa2946845cd111b
Azuki <73160631+azukiss@users.noreply.github.com>
2023-07-07 12:45:17 +0700

    Policy
    Policy

---
+++ app/Policies/PermissionPolicy.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Permission;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class PermissionPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return $user->can('view-any-permissions');
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Permission $permission): bool
+    {
+        return $user->can('view-permission');
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user): bool
+    {
+        return $user->can('update-permission');
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(): bool
+    {
+        return false;
+    }
+}
